25163dd8cf66333bf8399de350c628439ab57a0f
Arpit-mishra20 <arpit.202033@gmail.com>
2025-03-01 22:56:58 +0530

    added first project of assignment

---
+++ Springbootfirst-main/firstspringboot/src/main/java/com/demo/firstspringboot/collector/HelloController.java
@@ -0,0 +1,40 @@
+package com.demo.firstspringboot.collector;
+
+import com.demo.firstspringboot.UserDTO;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping
+    public String sayHello() {
+        return "Hello from BridgeLabz";
+    }
+}
+@RestController
+@RequestMapping("/hello")
+class HelloControllerMark {
+
+    @GetMapping("/query")
+    public String sayHello(@RequestParam(value = "name", defaultValue = "Mark") String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+}
+@RestController
+@RequestMapping("/hello")
+class HelloControllerMarkParams {
+    @GetMapping("/param/{name}")
+    public String sayHelloWithPathVariable(@PathVariable String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+}
+@RestController
+@RequestMapping("/hello")
+class HelloControllerPost {
+
+    @PostMapping("/post")
+    public String sayHelloWithPost(@RequestBody UserDTO user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
+    }
+}
